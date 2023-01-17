144ce7f218f3e3ba00376e50f88fce0ea822b866
Nikita Rogalenko <nikitalr@mail.ru>
2023-01-17 21:32:57 +0300

    helios config commented

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,6 +10,7 @@ plugins {
     id("org.springframework.boot") version "2.5.6"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     application
+    war
 }
 
 version = "1.0-SNAPSHOT"
